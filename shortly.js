89cb1edb5e484bf587d5cd72dd0396e37cc3c888
andrewsouthpaw <andrewsmith@alumni.stanford.edu>
2015-01-12 17:01:39 -0800

    pass first 7 tests regarding item storage

--- shortly.js
+++ shortly.js
@@ -2,6 +2,7 @@ var express = require('express');
 var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 
 var db = require('./app/config');
@@ -13,6 +14,29 @@ var Click = require('./app/models/click');
 
 var app = express();
 
+
+
+// Set authentication configuration
+
+// var sess = {
+//   secret: 'top secret',
+//   cookie: {},
+//   // secret: 'top secret',
+//   resave: false,
+//   saveUninitialized: true
+// };
+
+// if (app.get('env') === 'production') {
+//   app.set('trust proxy', 1) // trust first proxy
+//   sess.cookie.secure = true // serve secure cookies
+// }
+
+// app.use(session(sess));
+
+
+
+// Handle other stuff
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
@@ -23,29 +47,27 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', 
+app.get('/', restrict,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/create', 
+app.get('/create', restrict,
 function(req, res) {
   res.render('index');
 });
 
-app.get('/links', 
+app.get('/links', restrict,
 function(req, res) {
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links', 
+app.post('/links', restrict,
 function(req, res) {
   var uri = req.body.url;
-
   if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
     return res.send(404);
   }
 
@@ -78,7 +100,20 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
+function restrict(req, res, next) {
+  next();
+  // if (req.session.user) {
+  //   next();
+  // } else {
+  //   console.error('Access restricted. Redirecting to login page.');
+  //   res.redirect('/login');
+  // }
+}
 
+// app.get('/login', function(req, res) {
+//   req.session.user = 'logged in';
+//   res.send('login page');
+// });
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
